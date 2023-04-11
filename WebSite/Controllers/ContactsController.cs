49e93e376284dd8fb72ca38f4e55f7357017f76c
6N2P <umnik1985@mail.ru>
2023-04-11 12:09:37 +0500

    first commit

---
+++ WebSite/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                //Переадресация на главную страницу
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ WebSite/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSite.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage ="Обязательное поле")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Обязательное поле")]
+        public string Sername { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Обязательное поле")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Обязательное поле")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [Required(ErrorMessage = "Обязательное поле")]
+        [StringLength(2,ErrorMessage ="Сообщение немение 2х символов")]
+        public string Message { get; set; }
+    }
+}
